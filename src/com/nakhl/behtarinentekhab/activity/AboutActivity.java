8f473e7bbb29601ecca7f00542b171e780350458
Ahmad_livebits <airan2500@gmail.com>
2014-10-05 15:09:24 +0330

    add two new file

---
+++ src/com/nakhl/behtarinentekhab/activity/AboutActivity.java
@@ -0,0 +1,16 @@
+package com.nakhl.behtarinentekhab.activity;
+
+import android.os.Bundle;
+
+import com.nakhl.behtarinentekhab.R;
+
+public class AboutActivity extends FullScreenActivity {
+
+	//about activity
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_about);
+		
+	}
+}
